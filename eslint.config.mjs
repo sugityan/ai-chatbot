70bb446dcaf1b514a95d269b146d33e63af209ca
sugityan <baketu427@gmail.com>
2025-04-21 23:52:30 +0900

    Fix ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,22 @@
-import globals from "globals";
-import js from "@eslint/js";
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import nextPlugin from "@next/eslint-plugin-next";
+import globals from 'globals';
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import nextPlugin from '@next/eslint-plugin-next';
 
 export default [
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.es2021,
-        React: "writable",
-        JSX: "writable",
-        NodeJS: "writable",
+        React: 'writable',
+        JSX: 'writable',
+        NodeJS: 'writable',
       },
       parser: typescriptParser,
       parserOptions: {
@@ -23,44 +24,50 @@ export default [
           jsx: true,
         },
         ecmaVersion: 2021,
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     plugins: {
-      "@typescript-eslint": typescript,
+      '@typescript-eslint': typescript,
       react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-      "@next/next": nextPlugin,
+      'react-hooks': reactHooksPlugin,
+      '@next/next': nextPlugin,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
       // Core ESLint rules
-      "no-unused-vars": "off", // Disabled in favor of TypeScript version
-      "no-undef": "error",
-      "no-console": "warn",
-      
+      'no-unused-vars': 'off', // Disabled in favor of TypeScript version
+      'no-undef': 'error',
+      'no-console': 'warn',
+
       // TypeScript rules
-      "@typescript-eslint/no-unused-vars": ["warn", {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-      }],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/ban-ts-comment": ["error", {
-        "ts-expect-error": "allow-with-description",
-      }],
-      
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': 'allow-with-description',
+        },
+      ],
+
       // React rules
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
       // Next.js rules
-      "@next/next/no-img-element": "warn",
+      '@next/next/no-img-element': 'warn',
     },
   },
   js.configs.recommended,
