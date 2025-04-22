c32365eaf765d67826e57d36331b803078d17182
sugityan <baketu427@gmail.com>
2025-04-22 12:31:04 +0900

    delete test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
