270e088da23d81bd1e8990239b63f007fb5d1437
sugityan <baketu427@gmail.com>
2025-04-25 01:28:13 +0900

    test adding chat page

---
+++ src/app/(protected)/(chat)/chat/page.tsx
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { Chat } from "@/components/chat";
+import { DEFAULT_CHAT_MODEL } from "@/lib/ai/models";
+import { generateUUID } from "@/lib/utils";
+import { DataStreamHandler } from "@/components/data-stream-handler";
+
+export default async function Page() {
+  const id = generateUUID();
+
+  const cookieStore = await cookies();
+  const modelIdFromCookie = cookieStore.get("chat-model");
+
+  return (
+    <>
+      <Chat
+        key={id}
+        id={id}
+        initialMessages={[]}
+        selectedChatModel={modelIdFromCookie?.value || DEFAULT_CHAT_MODEL}
+        selectedVisibilityType="private"
+        isReadonly={false}
+      />
+      <DataStreamHandler id={id} />
+    </>
+  );
+}
